53ee590e9f063d450a76c344f4033983bd3c521b
Kavindu <kavinduthilan4@gmail.com>
2025-10-03 01:50:42 +0530

    change firebase credintials

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -5,12 +5,13 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB_Nwt3mRAb710loiMw7LmYvblp91vDmJ0",
-  authDomain: "cashtracker-12304.firebaseapp.com",
-  projectId: "cashtracker-12304",
-  storageBucket: "cashtracker-12304.firebasestorage.app",
-  messagingSenderId: "118950351113",
-  appId: "1:118950351113:web:08c19080bc71d5860ec3d8"
+  apiKey: "AIzaSyBmnZIldQJbJV3rr85kVfgUnSXyudE4GPI",
+  authDomain: "cashtracker-43486.firebaseapp.com",
+  projectId: "cashtracker-43486",
+  storageBucket: "cashtracker-43486.firebasestorage.app",
+  messagingSenderId: "518802998206",
+  appId: "1:518802998206:web:bd0e1a1942abf26bb52fef",
+  measurementId: "G-W1TYD27JL7"
 };
 
 // Initialize Firebase
